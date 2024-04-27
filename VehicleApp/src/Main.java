53d7202a1085b87c4a388f037cb4a4f332b775c3
iabzeet <iabzeet@gmail.com>
2024-04-27 13:59:01 +0530

    vehicle created

---
+++ VehicleApp/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[] args) {
+		
+		SUV fortuner = new SUV("Fortuner", false);
+		fortuner.move(40, 0);
+		fortuner.accelerate(20);
+		//fortuner.accelerate(-60);
+		System.out.println("Current Gear: " + fortuner.getCurrentGear());
+		System.out.println("Current Speed: " + fortuner.getCurrentSpeed());
+	}
+}
+
